c90db4e0510e4959cd75f092e0c1666f0d9b2b53
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2024-05-13 13:29:58 +0200

    public controler

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,19 @@
 <?php
+$getMaps = getAllMaps($db);
+$mapCount = count(getAllMaps($db));
+
+// LOGIN
+if (isset($_POST["userNameInp"], $_POST["userPassInp"])) {
+    $login = attemptUserLogin($db, $_POST["userNameInp"], $_POST["userPassInp"]);
+    if ($login === true) {
+            header ("Location: ?page=adminHome");
+        }else {
+        $errorMessage = "Saissisez correctement vos détails";
+    }
+        include('Location: ?page=homeBS&login');
+        exit();
+}
+
+if (isset($_GET["sort"])) {
+    $getMaps = getMapsByType($db, $_GET["sort"]);
+}
